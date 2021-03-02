24bae84c4b84ade9f19aa91a5009a9e8945e03da
Shavindra <shavindra@users.noreply.github.com>
2021-03-02 16:09:32 +0100

    Implement docker compose

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # infovis-24
 Created with CodeSandbox
+
+## Instructions
+
+1. Clone repository
+
+2. In the root directory run `docker-compose up`.
+
+3. Navigate to `http://localhost:5080`
--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ function initMain() {
   document.getElementById("app").innerHTML = `
   <h1>Hello Vanilla!</h1>
   <div>
-    We use the same configuration as Parcel to bundle this sandbox, you can find more
-    info about Parcel 
+    Info vis 24
     <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
   </div>
   `;
